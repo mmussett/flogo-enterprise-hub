c18a870e9a50841e38a51b19f3a5e12a649be4e6
TIBCO® Developer Hub <test@test.com>
2025-08-22 16:55:11 +0100

    Committed by TIBCO Developer Hub

---
+++ extensions/logger/trigger/logTailFollower/metadata.go
@@ -0,0 +1,33 @@
+package 
+
+import (
+	"github.com/project-flogo/core/data/coerce"
+)
+
+type Settings struct {
+	MyString string `md:"mystring,required"`
+}
+type HandlerSettings struct {
+	MyString  string `md:"myString"`
+	MyBoolean bool   `md:"myBoolean"`
+	MyInteger int    `md:"myInteger"`
+}
+
+type Output struct {
+	Output map[string]interface{} `md:"output"`
+}
+
+func (o *Output) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"output": o.Output,
+	}
+}
+
+func (o *Output) FromMap(values map[string]interface{}) error {
+	var err error
+	o.Output, err = coerce.ToObject(values["output"])
+	if err != nil {
+		return err
+	}
+	return nil
+}
