39c33b62d451b6b5c59a091ccba240248098c688
TIBCO® Developer Hub <test@test.com>
2025-08-29 11:44:21 +0100

    Committed by TIBCO Developer Hub

--- extensions/logger/src/activity/myActivity/metadata.go
+++ extensions/logger/src/activity/myActivity/metadata.go
@@ -6,6 +6,9 @@ type Settings struct {
 	
 	
     Fieldname1 string `md:"fieldName1,required"`
+		
+	
+    Fieldname2 float64 `md:"fieldName2,required"`
 	
 }
 
@@ -13,6 +16,9 @@ type Input struct {
 	
 	
     Inputfield1 string `md:"inputField1,required"`
+		
+	
+    Inputfield2 string `md:"inputField2,required"`
 	
 }
 
@@ -20,6 +26,9 @@ type Output struct {
 	
 	
     Outputfield1 string `md:"outputField1,required"`
+		
+	
+    Outputfield2 string `md:"outputField2,required"`
 	
 }
 
@@ -33,6 +42,12 @@ func (i *Input) FromMap(values map[string]interface{}) error {
 	
 	if err != nil {
 		return err
+	}	
+	
+	i. Inputfield2, err = coerce.ToString(values["inputField2"])
+	
+	if err != nil {
+		return err
 	}
 
 	return nil
@@ -43,6 +58,7 @@ func (i *Input) ToMap() map[string]interface{} {
 	return map[string]interface{}{
 
 		"inputField1": i.Inputfield1,
+		"inputField2": i.Inputfield2,
 	}
 
 }
@@ -57,6 +73,12 @@ func (o *Output) FromMap(values map[string]interface{}) error {
 	
 	if err != nil {
 		return err
+	}	
+	
+	o. Outputfield2, err = coerce.ToString(values["outputField2"])
+	
+	if err != nil {
+		return err
 	}
 
 	return nil
@@ -64,10 +86,10 @@ func (o *Output) FromMap(values map[string]interface{}) error {
 
 func (o *Output) ToMap() map[string]interface{} {
 
-
 	return map[string]interface{}{
 
-		"outputField1": o.Outputfield1,		
+		"outputField1": o.Outputfield1,
+		"outputField2": o.Outputfield2,		
 	}
 
 }
