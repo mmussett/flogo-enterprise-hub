e7d29420af79c2e68da7d001e957701021b64881
TIBCO® Developer Hub <test@test.com>
2025-08-29 15:52:05 +0100

    Committed by TIBCO Developer Hub

--- extensions/logger/src/trigger/logWatcher/metadata.go
+++ extensions/logger/src/trigger/logWatcher/metadata.go
@@ -25,16 +25,16 @@ type Output struct {
 }
 
 func (o *Output) ToMap() map[string]interface{} {
+	
 	return map[string]interface{}{
-		"output": o.Output,
+		
 	}
 }
 
 func (o *Output) FromMap(values map[string]interface{}) error {
 	var err error
-	o.Output, err = coerce.ToObject(values["output"])
-	if err != nil {
-		return err
-	}
+
+
+
 	return nil
 }
